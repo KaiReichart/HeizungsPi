57651e2514cdc995ca54171a55420531e5eef55b
Kai Reichart <kai@reichart.dev>
2020-10-29 13:27:28 +0100

    bugfixes

--- heizungsserver.py
+++ heizungsserver.py
@@ -1,14 +1,3 @@
 from app import app
 
-try: 
-    with open("/sys/class/gpio/export", "w") as f: 
-        f.write("23")
-except:
-    print("Error Exporting GPIO PIN.")
-
-try:
-    with open("/sys/class/gpio/gpio23/direction", "w") as f:     
-        f.write("out")
-except:
-    print("Error writing GPIO PIN direction.")
     
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+echo "23" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio23/direction
+
 export FLASK_APP=heizungsserver.py
 python3 -m flask run --host=0.0.0.0
