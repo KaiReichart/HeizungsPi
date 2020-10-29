72718203e109d193747c53af2075911a38bd7ef1
Kai Reichart <kai@reichart.dev>
2020-10-29 13:37:30 +0100

    try

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-echo "23" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio23/direction
+echo "23" > /sys/class/gpio/export || TRUE
+echo "out" > /sys/class/gpio/gpio23/direction || TRUE
 
 export FLASK_APP=heizungsserver.py
 python3 -m flask run --host=0.0.0.0
