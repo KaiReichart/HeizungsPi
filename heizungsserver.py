df8465fdac089624ed2041bf7cab5ec8ab975b52
Kai Reichart <kai@reichart.dev>
2020-10-29 12:35:33 +0100

    bug

--- heizungsserver.py
+++ heizungsserver.py
@@ -1,14 +1,14 @@
 from app import app
 
-with open("/sys/class/gpio/export", "w") as f: 
-    try:
+try: 
+    with open("/sys/class/gpio/export", "w") as f: 
         f.write("23")
-    except:
-        print("Error Exporting GPIO PIN.")
+except:
+    print("Error Exporting GPIO PIN.")
 
-with open("/sys/class/gpio/gpio23/direction", "w") as f: 
-    try:
+try:
+    with open("/sys/class/gpio/gpio23/direction", "w") as f:     
         f.write("out")
-    except:
-        print("Error writing GPIO PIN direction.")
+except:
+    print("Error writing GPIO PIN direction.")
     
